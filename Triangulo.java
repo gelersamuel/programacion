2cc9ce19312d080508b9e0d03f40b07cc7d3152c
gelersamuel <56076674+gelersamuel@users.noreply.github.com>
2020-12-16 11:26:36 +0100

    Add files via upload

---
+++ Triangulo.java
@@ -0,0 +1,26 @@
+
+package triangulo;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Triangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int base=4,alt=3, area;
+   area=base*alt/2;
+   System.out.println("El area del triangulo es:"+area);
+    
+    
+    
+    
+    
+    
+    }
+    
+}
