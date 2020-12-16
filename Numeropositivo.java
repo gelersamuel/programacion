bd77c8b73fcd2c6d2c8a0e7fabd804b2917f189f
gelersamuel <56076674+gelersamuel@users.noreply.github.com>
2020-12-16 11:29:26 +0100

    Add files via upload

---
+++ Numeropositivo.java
@@ -0,0 +1,15 @@
+
+package bol7_1;
+import javax.swing.*;
+/**
+ *
+ * @author Lenovo
+ */
+public class Numeropositivo {
+    
+    public void NumeroPositivo( int num){
+        if(num>0){
+            JOptionPane.showInputDialog("O numero " + num + " é positivo");
+        }
+}
+}
